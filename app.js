4854a08ddfb0c3ed3ee608572d7d6c02e8c2e5c7
perla serrano <perlaserrano2015@outlook.com>
2022-11-11 17:52:34 -0400

    validate form

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
+const regExp = /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/
+
+// const regExpObjeto = new RegExp("perlaweb","i");
+
+console.log(regExp.test("perlaserrano2015@outlook.com"));
 
 
+const alertName = document.getElementById('alertName');
 const formLearn = document.getElementById('formLearn');
 const inputs = document.querySelectorAll('#formLearn input');
+
+const alertError = (errores) =>{
+    errores.forEach((item) =>{
+        item.tipo.classList.remove("d-none");
+        item.tipo.textContent = item.msg;
+    })
+
+};
 
 function handlerChange(e) {
 }
@@ -12,7 +26,7 @@ inputs.forEach((input) =>{
 
 formLearn.addEventListener("submit", (e) =>{
     e.preventDefault();
-    
+
     let data = {};
 
     e.target.querySelectorAll("input").forEach((input) => {
@@ -20,10 +34,31 @@ formLearn.addEventListener("submit", (e) =>{
     })
 
     console.log(data.firstname);
-
-
+    console.log(data.lastname);
+    console.log(data.emailaddress);
+    console.log(data.password);
 
 });
+
+(function () {
+    'use strict'
+  
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+
+
 
 
 
